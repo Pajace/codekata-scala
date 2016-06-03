a983cc810a4b188b16e0e2bbf28065b62a1b5800
Pajace <Pajace@gmail.com>
2016-06-03 11:12:31 +0800

    Pass first test case

---
+++ src/main/scala/com/logdown/mycodetub/easy/FizzBuzz.scala
@@ -0,0 +1,9 @@
+package com.logdown.mycodetub.easy
+
+/**
+  * Created by pajace_chen on 2016/6/3.
+  */
+object FizzBuzz {
+    def getResult(number: Int) = "Fizz"
+
+}
---
+++ src/test/scala/FizzBuzzTest.scala
@@ -0,0 +1,13 @@
+import org.scalatest.{FlatSpec, Matchers}
+import com.logdown.mycodetub.easy.FizzBuzz
+
+/**
+  * Created by pajace_chen on 2016/6/3.
+  */
+class FizzBuzzTest extends FlatSpec with Matchers {
+
+    "FizzBuzz" should " return Fizz, if number is dividable by 3 " in {
+        FizzBuzz.getResult(3) should be ("Fizz")
+    }
+
+}
