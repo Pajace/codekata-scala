e61c5ec62df86fc1d38fcb68d92fd2cb512127b9
Pajace <Pajace@gmail.com>
2016-06-03 11:17:07 +0800

    pass 3rd test

--- src/main/scala/com/logdown/mycodetub/easy/FizzBuzz.scala
+++ src/main/scala/com/logdown/mycodetub/easy/FizzBuzz.scala
@@ -4,5 +4,8 @@ package com.logdown.mycodetub.easy
   * Created by pajace_chen on 2016/6/3.
   */
 object FizzBuzz {
-    def getResult(number: Int) = if (number%3==0) "Fizz"
+    def getResult(number: Int) = (number % 3 == 0, number % 5 == 0) match {
+        case (true, _) => "Fizz"
+        case (_, true) => "Buzz"
+    }
 }
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -6,10 +6,14 @@ import com.logdown.mycodetub.easy.FizzBuzz
   */
 class FizzBuzzTest extends FlatSpec with Matchers {
 
-    "FizzBuzz" should " return Fizz, if number is dividable by 3 " in {
+    "FizzBuzz" should "return Fizz, if number is dividable by 3 " in {
         FizzBuzz.getResult(3) should be ("Fizz")
         FizzBuzz.getResult(6) should be ("Fizz")
     }
 
+    it should "return Buzz, if the number is dividable by 5" in {
+        FizzBuzz.getResult(5) should be ("Buzz")
+        FizzBuzz.getResult(10) should be ("Buzz")
+    }
 
 }
