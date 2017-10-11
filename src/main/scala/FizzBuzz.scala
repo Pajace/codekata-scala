dc6c36bf3c5051432f26cdf5f3b62ed12101027a
pajace_chen <pajace_chen@htc.com>
2017-10-11 08:14:22 +0800

    update FizzBuzz

--- src/main/scala/FizzBuzz.scala
+++ src/main/scala/FizzBuzz.scala
@@ -2,10 +2,25 @@
   * Created by pajace_chen on 2016/6/3.
   */
 object FizzBuzz {
-    def getResult(number: Int) = (number % 3, number % 5) match {
-        case (0, 0) => "FizzBuzz"
-        case (0, _) => "Fizz"
-        case (_, 0) => "Buzz"
-        case (_) => number.toString
+
+    implicit class FizzBuzzEnhancement(input: Int) {
+
+        def isFizz: Int => Boolean = x => x % 3 == 0
+
+        def isBuzz: Int => Boolean = x => x % 5 == 0
+
+        def isFizzBuzz: Int => Boolean = x => isFizz(x) && isBuzz(x)
+
+        def determineFizzBuzz: String = {
+            val result =
+                if (isFizzBuzz(input)) "FizzBuzz"
+                else if (isFizz(input)) "Fizz"
+                else if (isBuzz(input)) "Buzz"
+                else input.toString
+            result
+        }
     }
+
+    def calcResult: Int => String = input => input.determineFizzBuzz
+
 }
--- src/test/scala/FizzBuzzTest.scala
+++ src/test/scala/FizzBuzzTest.scala
@@ -6,24 +6,24 @@ import org.scalatest.{FlatSpec, Matchers}
 class FizzBuzzTest extends FlatSpec with Matchers {
 
     "FizzBuzz" should "return Fizz, if number is dividable by 3 " in {
-        FizzBuzz.getResult(3) should be ("Fizz")
-        FizzBuzz.getResult(6) should be ("Fizz")
+        FizzBuzz.calcResult(3) should be ("Fizz")
+        FizzBuzz.calcResult(6) should be ("Fizz")
     }
 
     it should "return Buzz, if the number is dividable by 5" in {
-        FizzBuzz.getResult(5) should be ("Buzz")
-        FizzBuzz.getResult(10) should be ("Buzz")
+        FizzBuzz.calcResult(5) should be ("Buzz")
+        FizzBuzz.calcResult(10) should be ("Buzz")
     }
 
     it should "return FizzBuzz if the number is dividable by 15" in {
-        FizzBuzz.getResult(15) should be ("FizzBuzz")
-        FizzBuzz.getResult(30) should be ("FizzBuzz")
+        FizzBuzz.calcResult(15) should be ("FizzBuzz")
+        FizzBuzz.calcResult(30) should be ("FizzBuzz")
     }
 
     it should "return the same number if no other requirement is fulfilled" in {
-        FizzBuzz.getResult(1) should be ("1")
-        FizzBuzz.getResult(2) should be ("2")
-        FizzBuzz.getResult(4) should be ("4")
+        FizzBuzz.calcResult(1) should be ("1")
+        FizzBuzz.calcResult(2) should be ("2")
+        FizzBuzz.calcResult(4) should be ("4")
     }
 
 }
